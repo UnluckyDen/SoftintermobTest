2326de6cb9be683ebeecfd821213adc9d18f9e03
UnluckyDen <dsgladkih01@gmail.com>
2025-08-12 11:31:43 +0300

    Update HeroView.cs

--- Assets/Presentation/Views/HeroView.cs
+++ Assets/Presentation/Views/HeroView.cs
@@ -12,15 +12,45 @@ namespace Presentation.Views
         private Label _levelText;
         private Label _strengthText;
 
+        private int _level;
+        private int _strength;
+
         private void Start()
         {
             _root = _uiDocument.rootVisualElement;
             _levelText = _root.Q<Label>("LevelText");
             _strengthText = _root.Q<Label>("StrengthText");
+
+            UpdateLevelText();
+            UpdateStrengthText();
         }
 
-        public void UpdateLevel(int level) => _levelText.text = $"Hero level: {level}";
+        public void UpdateLevel(int level)
+        {
+            _level = level;
+            UpdateLevelText();
+        }
 
-        public void UpdateStrength(int strength) => _strengthText.text = $"Hero strength: {strength}";
+        public void UpdateStrength(int strength)
+        {
+            _strength = strength;
+            UpdateStrengthText();
+        }
+
+        private void UpdateLevelText()
+        {
+            if (_levelText == null)
+                return;
+            
+            _levelText.text = $"Hero level: {_level}";
+        }
+
+        private void UpdateStrengthText()
+        {
+            if (_strengthText == null)
+                return;
+            
+            _strengthText.text = $"Hero strength: {_strength}";
+        }
     }
 }
